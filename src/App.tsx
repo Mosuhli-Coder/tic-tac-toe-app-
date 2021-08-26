d052f61dfea39cb9e0f2249ab30136d88c509c36
Mosuhli-Coder <drmosuhli@gmail.com>
2021-08-26 12:03:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Tic Tac Toe App by Teboho Mosuhli</p>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
+
+// Game.tsx - Have board and logic( status, )
+// Board.tsx - Display game status ()
+// Square.tsx - x o empty, clicks or actions 
